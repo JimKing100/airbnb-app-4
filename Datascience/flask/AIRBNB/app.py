010a30b02784d5c0600c99dad2f2aea992ee38c4
JimKing100 <52686228+JimKing100@users.noreply.github.com>
2019-12-19 09:57:33 -0800

    Add files via upload

--- Datascience/flask/AIRBNB/app.py
+++ Datascience/flask/AIRBNB/app.py
@@ -2,9 +2,10 @@
 
 from decouple import config
 from flask import Flask, render_template
-from AIRBNB.models import DB, Record
-# import requests
-import openaq
+from AIRBNB.models import DB
+
+from joblib import load
+import pandas as pd
 
 
 def create_app():
@@ -18,19 +19,42 @@ def create_app():
     # Routes determine location
     @app.route("/")
     def home():
+
+        month = 12
+        year = 19
+        property_type = 'House'
+        room_type = 'Entire home/apt'
+        neighbourhood = 'East Downtown'
+        accommodates = 4
+        bedrooms = 2
+        bathrooms = 2
+        beds = 2
+        out_string = 'The predicted price for a ' + \
+                     property_type.lower() + ' in the ' + \
+                     neighbourhood + ' neighborhood '\
+                     'accommodating ' + str(accommodates) + \
+                     ' people in ' + str(bedrooms) + ' bedrooms ' + \
+                     str(bathrooms) + ' baths is: '
+
+        df = pd.DataFrame(
+            columns=['month', 'year', 'property_type', 'room_type',
+                     'neighbourhood', 'accommodates', 'bedrooms',
+                     'bathrooms', 'beds'],
+            data=[[month, year, property_type, room_type, neighbourhood,
+                   accommodates, bedrooms, bathrooms, beds]]
+        )
+
+        pipeline = load('pipeline.joblib')
+        y_pred_log = pipeline.predict(df)
+        y_pred = y_pred_log[0]
+
+        result = out_string + f'${y_pred:,.0f}'
+
+        return render_template('home.html', title='Prediction', result=result)
+
+    @app.route('/reset')
+    def reset():
         DB.drop_all()
         DB.create_all()
-
-        api = openaq.OpenAQ()
-        status, body = api.measurements(city='Los Angeles', parameter='pm25')
-
-        for i in range(0, 100):
-            date1 = body['results'][i]['date']['utc']
-            value1 = body['results'][i]['value']
-            db_record = Record(id=i, datetime=date1, value=value1)
-            DB.session.add(db_record)
-
-        DB.session.commit()
-        records = Record.query.filter(Record.value > 10)
-        return render_template('home.html', title=date1, records=records)
+        return render_template('db.html', title='DB Reset')
     return app
